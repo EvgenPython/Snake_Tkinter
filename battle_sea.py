58669f65623a1daedadff81f601accec80e23fe4
keba_e <fsdfsd@fdfs.com>
2025-04-24 12:37:45 +0300

    5

--- battle_sea.py
+++ battle_sea.py
@@ -19,9 +19,9 @@ class Cell:
         x1, y1, = x * CELL_SIZE, y * CELL_SIZE
         x2, y2 = x1 * CELL_SIZE, y1 * CELL_SIZE
         self.rect = canvas.create_rectangle(x1, y1, x2, y2, fill="blue", outline="white")
-        # self.canvas.tag_bin(self.rect, "<Button-1>", self.on_click)
+        self.canvas.tag_bind(self.rect, "<Button-1>", self.on_click)
 
-    def on_click(self):
+    def on_click(self, event):
         if self.click_callback and self.owner == "comp":
             self.click_callback(self)
 
@@ -38,6 +38,43 @@ class Board:
         self.owner = owner
         self.grid = [[Cell(self.canvas, x, y, owner, click_callback) for y in range(GRID_SIZE)] for x in
                      range(GRID_SIZE)]
+
+    def place_all_ships(self):
+        for size, count in SHIP_SIZES.items():
+            for _ in range(count):
+                placed = False
+                while not placed:
+                    placed = self.try_place_ship(size)
+
+    def try_place_ship(self, size):
+        orientation = random.choice(["H", "V"])
+        if orientation == "H":
+            x = random.randint(0, GRID_SIZE - size)
+            y = random.randint(0, GRID_SIZE - 1)
+            coords = [(x + i, y) for i in range(size)]
+        else:
+            x = random.randint(0, GRID_SIZE - 1)
+            y = random.randint(0, GRID_SIZE - size)
+            coords = [(x, y + i) for i in range(size)]
+
+        if any(self.grid[cx][cy].has_ship for cx, cy in coords):
+            return False
+
+        for cx, cy in coords:  # [(1,2),  (3,4), (4,5)]
+            for dx in [-1, 0, 1]:
+                for dy in [-1, 0, 1]:
+                    nx, ny = cx + dx, cy + dy
+                    if 0 <= nx < GRID_SIZE and 0 <= ny < GRID_SIZE:
+                        if self.grid[nx][ny].has_ship and (nx, ny) not in coords:
+                            return False
+        for cx, cy in coords:
+            self.grid[cx][cy].has_ship = True
+            if self.owner == "player":
+                self.grid[cx][cy].canvas.itemconfig(self.grid[cx][cy].rect, fill="green")
+        return True
+
+    def all_ships_destroy(self):
+        all()
 
 class SeaBattleGame:
     def __init__(self, root):
--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-combos = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
-for a,b,c in combos:
-    print(a, b,c)
+self.grid = [[Cell(self.canvas, x, y, owner, click_callback) for y in range(GRID_SIZE)] for x in range(GRID_SIZE)]
+
+
+# res = [i for i in range(0, 5)]
+# print(res)
+#
+# res_2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# for i in range(3):
+#     for j in range(3):
+#         print(res_2[i][j], end=" ")
+
+res_3 = [[i for i in range(3, 9)] for j in range(3)]
+print(res_3)
